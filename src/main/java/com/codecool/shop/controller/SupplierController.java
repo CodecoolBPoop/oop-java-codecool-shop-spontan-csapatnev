44cbba10a86a466c5785d715c456ce9c31dbf9b5
mogyorosibalin <mogyorosibalin@gmail.com>
2018-07-17 10:01:14 +0200

    Added SupplierController class, implementation needed

---
+++ src/main/java/com/codecool/shop/controller/SupplierController.java
@@ -0,0 +1,14 @@
+package com.codecool.shop.controller;
+
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class SupplierController extends BaseController {
+
+    @Override
+    void addPlusContext(WebContext context, HttpServletRequest req) {
+
+    }
+
+}

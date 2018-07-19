453d501c5ec11d111de240a611c340605edefcf2
Fábián János <fabian.janos93@gmail.com>
2018-07-19 08:47:10 +0000

    ajax working

--- src/main/java/com/codecool/shop/controller/ShoppingCartController.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartController.java
@@ -7,6 +7,8 @@ import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.model.ShoppingCart;
+import jdk.nashorn.internal.ir.debug.JSONWriter;
+import org.json.simple.JSONObject;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -16,17 +18,50 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/shopping-cart"})
 public class ShoppingCartController extends BaseController {
 
+    private static final java.util.stream.Collectors Collectors = ;
     private final String ACTION_ADD = "add";
     private final String ACTION_REMOVE = "remove";
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        String json = "";
+        if(br != null){
+            json = br.readLine();
+        }
+
+        if (action.equals(ACTION_ADD)) {
+            ShoppingCart.add(session, productId);
+        } else if (action.equals(ACTION_REMOVE)) {
+            if (req.getParameter("all") != null) {
+                if (req.getParameter("all").equals("true")) {
+                    ShoppingCart.remove(session, productId, true);
+                    return;
+                }
+            }
+            ShoppingCart.remove(session, productId, false);
+        }*/
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        JSONObject answer = new JSONObject();
+
+        resp.getWriter().write(answer.toString());
+    }
+}
+
+/*    @Override
     void addPlusContext(WebContext context, HttpServletRequest req) {
         context.setVariable("products", productDataStore.getAll());
         String action = req.getParameter("action");
@@ -46,4 +81,4 @@ public class ShoppingCartController extends BaseController {
             }
         }
     }
-}
+}*/
--- src/main/webapp/static/js/dataHandler.js
+++ src/main/webapp/static/js/dataHandler.js
@@ -13,15 +13,29 @@ $('#shoppingCartHandler').on('click', function(e) {
     $('.shopping-cart-content').toggleClass('opened');
 });
 
-$('.data-handler-button').on('click', function (ev) {ev.preventDefault();}
+$('.data-handler-button').on('click', function (ev) {ev.preventDefault();
+    var data = this.href.split('?')[1].split("&");
+    var action = data[0].split('=')[1];
+    var id = data[1].split('=')[1];
+    var removeAll = false;
+    if (data.length > 2){
+        removeAll = data[2].split('=')[1];
+    }
+
     $.ajax({
-            url: "/shoppingcart",
-            success: function () {
-
-            }
+        type: "GET",
+        url: "/shopping-cart",
+        data: JSON.stringify({
+                action: action,
+                id: id,
+                removeAll: removeAll
+            }),
+        dataType : 'json',
+        success: function () {
+            alert('sikerült!');
         }
-
+        }
     )
-)
+});
 
 console.log( "ready!" );

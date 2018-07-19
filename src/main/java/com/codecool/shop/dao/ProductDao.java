5dcd263e27487dddc76d490ec76730189326d465
mogyorosibalin <mogyorosibalin@gmail.com>
2018-07-19 09:30:35 +0200

    Java routing added and checking and getting the clicked product

--- src/main/java/com/codecool/shop/dao/ProductDao.java
+++ src/main/java/com/codecool/shop/dao/ProductDao.java
@@ -4,12 +4,13 @@ import com.codecool.shop.model.Supplier;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 
+import javax.xml.bind.Element;
 import java.util.List;
 
 public interface ProductDao {
 
     void add(Product product);
-    Product find(int id);
+    Product find(int id) throws ElementNotFoundException;
     void remove(int id);
 
     List<Product> getAll();

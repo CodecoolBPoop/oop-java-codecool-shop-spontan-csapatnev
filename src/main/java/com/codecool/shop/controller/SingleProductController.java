3c78cae30a18c4f5f26fb29c2e2ec00f1082f136
mogyorosibalin <mogyorosibalin@gmail.com>
2018-08-01 10:03:16 +0200

    Unneccessary import removed and comments deleted

--- src/main/java/com/codecool/shop/controller/SingleProductController.java
+++ src/main/java/com/codecool/shop/controller/SingleProductController.java
@@ -1,24 +1,12 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ElementNotFoundException;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.model.Product;
-import com.codecool.shop.model.ProductCategory;
-import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @WebServlet(urlPatterns = {"/products/*"})
 public class SingleProductController extends BaseController {
